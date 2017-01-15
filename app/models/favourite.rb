06e56b91aa7fdabe04f34f97f86a62ac6aaf61df
Alex Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-15 18:17:10 +0200

    fix favourites uniqueness

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -4,7 +4,7 @@ class Favourite < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :favourable, :user
-  validates_uniqueness_of :favourable_id, scope: [:favourable_id, :favourable_type]
+  validates_uniqueness_of :user_id, scope: [:favourable_id, :favourable_type]
 
   default_scope { order(created_at: :desc) }
 
