5fd84ae2e4a23ddf412c5d4e6d20d97d276608ce
Alex Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-15 14:11:25 +0200

    fix specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username { Faker::Internet.user_name }
+    username { Faker::Internet.user_name(4) }
     password { 'q1w2e3r4' }
     password_confirmation { 'q1w2e3r4' }
   end
